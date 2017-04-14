750c48de35c4b1c57ac3b284c2c4c0bc4b55ef8b
kimprzybylski <kim.przybylski@student.pxl.be>
2017-04-14 18:26:53 +0200

    .net versie

---
+++ Crypto Program MVC/Crypto Program MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Crypto_Program_MVC.MvcApplication" Language="C#" %>
---
+++ Crypto Program MVC/Crypto Program MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Crypto_Program_MVC.Startup))]
+namespace Crypto_Program_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
